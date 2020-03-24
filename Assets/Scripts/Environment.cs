423485e7b45174cf9ef46f4639438b4577c3ec84
coolasjake <coolasjake8@gmail.com>
2020-03-24 15:17:20 +1100

    Init

---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Environment : MonoBehaviour
+{
+    public RawEnvironment Env = new RawEnvironment();
+    public GameObject[,] cells = new GameObject[0, 0];
+    public GameObject cellPre;
+    private float cellSize = 80;
+    private Canvas canvas;
+
+    private GameObject Cell(Vector2Int coords)
+    {
+        return cells[coords.x, coords.y];
+    }
+
+    void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+        Env = Save.LoadEnvironment("Assets/RobotNav-test.txt");
+        GenerateEnvVisuals();
+    }
+
+    private void GenerateEnvVisuals()
+    {
+        cells = new GameObject[Env.gridSize.y, Env.gridSize.x];
+        Vector2 startPos = new Vector2(cellSize, cellSize + cellSize * Env.gridSize.x);
+        for (int row = 0; row < Env.gridSize.y; ++row)
+        {
+            for (int col = 0; col < Env.gridSize.x; ++col)
+            {
+                cells[row, col] = Instantiate(cellPre, new Vector3(startPos.x + (row * cellSize), startPos.y - (col * cellSize)), new Quaternion(), canvas.transform);
+                cells[row, col].GetComponent<Image>().rectTransform.sizeDelta = new Vector2(cellSize, cellSize);
+            }
+        }
+
+        Cell(Env.startPos).GetComponent<Image>().color = Color.red;
+
+        foreach (Vector2Int goal in Env.goalsPos)
+            Cell(goal).GetComponent<Image>().color = Color.green;
+
+        foreach (Rect wall in Env.wallRects)
+        {
+            int i = 0;
+            for (int x = (int)wall.x; i < (int)wall.width; ++x)
+            {
+                ++i;
+                int j = 0;
+                for (int y = (int)wall.y; j < (int)wall.height; ++y)
+                {
+                    ++j;
+                    cells[x, y].GetComponent<Image>().color = Color.black;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Save : MonoBehaviour
+{
+
+    /// <summary> List of directories for the units in the players collection. </summary>
+    public static List<string> Collection = new List<string>();
+    public static string CurrentNodeName;
+    public static int PlayerLevel = 3;
+    List<string> lines = new List<string>();
+
+    public static RawEnvironment LoadEnvironment(string path)
+    {
+        if (File.Exists(path))
+        {
+            RawEnvironment RE = new RawEnvironment();
+            List<string> lines = new List<string>();
+            StreamReader SR = new StreamReader(path);
+            while (!SR.EndOfStream)
+                lines.Add(SR.ReadLine());
+
+            if (lines.Count < 3)
+            {
+                Debug.LogError("Environment file incomplete");
+                return new RawEnvironment();
+            }
+            
+            RE.gridSize = lines[0].StringToV2();
+            RE.startPos = lines[1].StringToV2();
+            string[] goals = lines[2].Split('|');
+            RE.goalsPos = new List<Vector2Int>();
+            foreach (string G in goals)
+                RE.goalsPos.Add(G.StringToV2());
+            lines.RemoveRange(0, 3);
+
+            if (lines.Count == 0)
+            {
+                Debug.LogWarning("No walls in Environment");
+                return new RawEnvironment();
+            }
+
+            RE.wallRects = new List<Rect>();
+            foreach (string wall in lines)
+                RE.wallRects.Add(wall.StringToRect());
+
+            return RE;
+        }
+        return new RawEnvironment();
+    }
+}
+
+[Serializable]
+public class RawEnvironment
+{
+    public Vector2Int gridSize = Vector2Int.zero;
+    public Vector2Int startPos = Vector2Int.zero;
+    public List<Vector2Int> goalsPos = new List<Vector2Int>();
+    public List<Rect> wallRects = new List<Rect>();
+}
